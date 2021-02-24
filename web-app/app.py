8d41609c022ac18b457d59fb9c55fe0bbd8295aa
Cynmil <hyunkok1@uci.edu>
2021-02-23 20:31:36 -0800

    added basic textform on the web application

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("home.html")
+
+@app.route('/', methods=['POST'])
+def form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 @app.route('/about')
 def about():
